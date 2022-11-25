17485e57471c4c4e435d6e5b8915c7f4c1985396
MartaPatrycja <105316228+MartaPatrycja@users.noreply.github.com>
2022-11-25 17:23:14 +0100

    updated styling

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,24 +6,22 @@ export default function WeatherInfo(props) {
   return (
     <div className="card-body-daily">
       <h1>{props.info.name}</h1>
-      <ul>
-        <li>
+      <ul className="row d-flex justify-content-between">
+        <li id="hour-top">
           <DateFormat day={props.info.date} />
         </li>
         <li className="text-capitalize">{props.info.description}</li>
       </ul>
 
-      <div className="row">
-        <div className="col-6">
+      <div className="row d-flex justify-content-between">
           <div className="clearfix mt-3">
             <WeatherIcon code={props.info.icon} size={64} />
-            <span className="temperature ms-2">
+            <span className="ms-2" id="temperature">
               {Math.round(props.info.temp)}
               <span className="unit">°C</span>
             </span>
-          </div>
         </div>
-        <div className="col-6">
+        <div className="additional-parameters d-flex justify-content-between">
           <ul className="mt-3">
             <li>Humidity: {props.info.humidity}%</li>
             <li>Wind: {Math.round(props.info.wind)} mph</li>
