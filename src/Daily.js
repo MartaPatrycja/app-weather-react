ae15dab4b00dd0e39923ff25b080b8f00364864a
MartaPatrycja <105316228+MartaPatrycja@users.noreply.github.com>
2022-11-26 03:55:20 +0100

    UX fixes

--- src/Daily.js
+++ src/Daily.js
@@ -19,8 +19,8 @@ export default function Daily(props) {
   }
   return (
     <div>
-      <div className="img-weather-small">{day()}</div>
-      <WeatherIcon code={props.data.weather[0].icon} size={24} />
+      <div className="img-weather-small row d-flex justify-content-between">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={50} />
       <div className="card-title-daily">
         <span className="temperatures-daily">{maxTemperature()} </span>
         <span className="daily-temperature-min">{minTemperature()}</span>
