a5034854e15ad1319b20a734fa6d40818c3f9b5f
MartaPatrycja <105316228+MartaPatrycja@users.noreply.github.com>
2022-11-27 22:34:24 +0100

    changd size of small weather icons

--- src/Daily.js
+++ src/Daily.js
@@ -20,7 +20,7 @@ export default function Daily(props) {
   return (
     <div>
       <div className="card-title-daily row d-flex justify-content-between mx-auto">{day()}</div>
-      <WeatherIcon code={props.data.weather[0].icon} size={50} />
+      <div className="row d-flex justify-content-between mx-auto" id="small-icon"><WeatherIcon code={props.data.weather[0].icon} size={60} /></div>
       <div className="card-title-daily mx-auto">
         <span className="temperatures-daily  mx-auto">{maxTemperature()} | </span>
         <span className="daily-temperature-min  mx-auto">{minTemperature()}</span>
