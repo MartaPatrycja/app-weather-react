bbfb99ed02f22f561ce27aa8c932e3917c374960
MartaPatrycja <105316228+MartaPatrycja@users.noreply.github.com>
2023-02-09 21:26:04 +0100

    updated the max-width

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,16 +5,16 @@ import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="card-city justify-content-between mx-auto p-2" id="city">
-      <h1 className="mx-auto">{props.info.name}</h1>
-      <ul className="row d-flex justify-content-between mx-auto">
+    <div id="city">
+      <h1>{props.info.name}</h1>
+      <ul>
         <li className="p-2" id="hour-top">
           <DateFormat day={props.info.date} />
         </li> 
         <li className="p-2 text-capitalize" id="description">{props.info.description}</li>
       </ul> 
 
-         <ul className="d-flex pt-1">
+         <ul className="d-flex">
             <li className="mx-auto"><WeatherIcon code={props.info.icon} size={100} /></li>
             <li className="mx-auto"><WeatherTemperature celsius={props.info.temp}/></li>
         </ul>
